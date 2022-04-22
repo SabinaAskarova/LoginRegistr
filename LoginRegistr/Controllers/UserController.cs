ac4f31a4f1791769ee1617e28c4f9c53b28789ef
SebineEskerova SabinaAskarova <sabinaaskarova56@gmail.com>
2022-04-22 10:34:24 +0400

    loginregistr

---
+++ LoginRegistr/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using LoginRegistr.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LoginRegistr.Controllers
+{
+    public class UserController : Controller
+    {
+        public static List<User> users = new List<User>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Registr(string username, string password, string email)
+        {
+            User user = new User()
+            {
+                Username = username,
+                Password = password,
+                Email = email
+            };
+            users.Add(user);
+
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Login(string username,string password)
+        {
+            foreach(User user in users)
+            {
+                if(user.Username==username && user.Password == password)
+                {
+                    return View();
+                }
+            }
+
+            return RedirectToAction("Error1");
+        }
+       public IActionResult Error1()
+        {
+            return View();
+        }
+
+        public IActionResult RegistrForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginRegistr/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginRegistr.Models
+{
+    public class User
+    {
+
+        public string Username;
+        public string Password;
+        public string Email;
+
+     
+    }
+}
